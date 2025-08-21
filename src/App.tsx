cd12e255b3b0bfdf7f9cdff82fd2ed8efde9a72b
evi-ken-facemona <36413519+kenny-kong-facemo@users.noreply.github.com>
2025-08-21 11:09:34 +0100

    matter.js
    matter.js

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PachinkoMatter from "./components/PachinkoMatter";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Pachinko — Matter.js + TypeScript</h1>
+      <PachinkoMatter />
+    </div>
+  );
+}
