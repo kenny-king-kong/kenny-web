530bcf413058fa2a04512f10946846aaafd3eddb
evi-ken-facemona <36413519+kenny-kong-facemo@users.noreply.github.com>
2025-08-23 16:31:33 +0100

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/guide/static-deploy
 export default defineConfig({
-  base: '/kenny-web/', // <-- IMPORTANT for GitHub Pages
+  base: '/kenny-web/',
+  build: {
+    outDir: 'docs'
+  }
 })
