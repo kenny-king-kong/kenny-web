99bfcfba84a45161573b55f615b387ed56a5ba67
evi-ken-facemona <36413519+kenny-kong-facemo@users.noreply.github.com>
2025-08-23 13:29:12 +0100

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/guide/static-deploy
 export default defineConfig({
-  base: '/REPO_NAME/', // <-- IMPORTANT for GitHub Pages
+  base: '/kenny-web/', // <-- IMPORTANT for GitHub Pages
 })
